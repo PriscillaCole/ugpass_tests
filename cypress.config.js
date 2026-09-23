6064c0597abd2fb6d2d5f4ae4ff5351ac411b02c
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2026-09-23 12:22:13 +0300

    Add Cypress GitHub Actions workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'sqfzr9',
   e2e: {
-    baseUrl: 'https://sign.ugpass.go.ug',
+    baseUrl: 'https://stgsign.ugpass.go.ug',
 
     defaultCommandTimeout: 20000,
     pageLoadTimeout: 120000,
